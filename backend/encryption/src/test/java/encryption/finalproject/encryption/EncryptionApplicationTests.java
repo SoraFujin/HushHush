390a099ba77682ad5a2260acaf1a9efa678e9ee5
SoraFujin <ahmaddiab29072002@gmail.com>
2025-01-19 00:13:05 +0200

    initial commit

---
+++ backend/encryption/src/test/java/encryption/finalproject/encryption/EncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package encryption.finalproject.encryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
